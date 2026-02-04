11620b576cd510cf3d347dd41e769afc10a4e1b0
Stephan Kugler <stephan.kugler@adesso.de>
2026-02-04 15:02:43 +0100

    feat: calculate future real value accounting for inflation

--- GO-BASICS/investment_calculator.go
+++ GO-BASICS/investment_calculator.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmount float64 = 1000
 	expectedReturnRate := 5.5
 	years := 10.0
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
