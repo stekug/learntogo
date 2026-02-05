e785af948a917c7312cc59ca670880a174420f68
Stephan Kugler <stephan.kugler@adesso.de>
2026-02-05 11:13:34 +0100

    refactor: rearrange input prompts for investment calculator for better user experience

--- GO-BASICS/investment_calculator.go
+++ GO-BASICS/investment_calculator.go
@@ -7,11 +7,18 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
-	years := 10.0
+	var investmentAmount float64
+	var years float64
 	expectedReturnRate := 5.5
 
+	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
