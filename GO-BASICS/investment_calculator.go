62c45df4cc758c2759c2dbec52f1feb8cc6aa5c2
Stephan Kugler <stephan.kugler@adesso.de>
2026-02-09 09:01:14 +0100

    refactor: enhance output formatting for future value calculations in investment calculator

--- GO-BASICS/investment_calculator.go
+++ GO-BASICS/investment_calculator.go
@@ -23,7 +23,10 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
+	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Real Value (adjusted for Inflation): %.1f\n", futureRealValue)
 	// fmt.Println("Future Value:", futureValue)
-	fmt.Printf("Future Value: %.0f\nFuture Real Value (adjusted for Inflation): %.0f", futureValue, futureRealValue)
+	// fmt.Printf("Future Value: %.1f\nFuture Real Value (adjusted for Inflation): %.1f", futureValue, futureRealValue)
 	// fmt.Println("Future Real Value (adjusted for Inflation)", futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
 }
