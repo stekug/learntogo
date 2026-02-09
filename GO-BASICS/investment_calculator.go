37e6ab014143f248b7a5d7eb47d337994878a091
Stephan Kugler <stephan.kugler@adesso.de>
2026-02-09 09:34:49 +0100

    refactor: simplify return values in calculateFutureValues function

--- GO-BASICS/investment_calculator.go
+++ GO-BASICS/investment_calculator.go
@@ -41,8 +41,9 @@ func outputText(text string) {
 	fmt.Print(text)
 }
 
-func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
-	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	rfv := fv / math.Pow(1+inflationRate/100, years)
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (fv float64, rfv float64) {
+	fv = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	rfv = fv / math.Pow(1+inflationRate/100, years)
 	return fv, rfv
+	// return
 }
