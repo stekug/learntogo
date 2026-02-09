c33b9af51ecf68864516590c0d9e1c1c3b493ab5
Stephan Kugler <stephan.kugler@adesso.de>
2026-02-09 14:43:02 +0100

    feat: implement basic banking functionality with balance check, deposit, and withdrawal options

---
+++ GO-BASICS-STRUCTURE/bank.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	var accountBalance = 1000.0
+
+	fmt.Println("Welcome to Go-Bank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit Money")
+	fmt.Println("3. Withdraw Money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	// wantsCheckBalance := choice == 1
+
+	if choice == 1 {
+		fmt.Println("Your balance is: ", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+
+		if depositAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0.")
+			return
+		}
+
+		accountBalance += depositAmount // accountBalance = accountBalance + depositBalance
+		fmt.Println("Balance updated. New Amount: ", accountBalance)
+	} else if choice == 3 {
+		fmt.Print("Withdrawal amount: ")
+		var withdrawalAmount float64
+		fmt.Scan(&withdrawalAmount)
+
+		if withdrawalAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0.")
+			return
+		}
+
+		if withdrawalAmount > accountBalance {
+			fmt.Println("Invalid amount. You cant withdraw more than you have.")
+			return
+		}
+
+		accountBalance -= withdrawalAmount
+		fmt.Println("Balance updated. New Amount: ", accountBalance)
+	} else {
+		fmt.Print("Goodbye!")
+		return
+	}
+}
